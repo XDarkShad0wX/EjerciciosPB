12b46bd1a8486a803a9ed102c1ef37ed1b00073f
Daniel Valle <danielvalleortiz@gmail.com>
2024-02-18 16:10:27 -0500

    Ejercicio3

--- Ejercicio3.java
+++ Ejercicio3.java
@@ -3,16 +3,13 @@ public class Ejercicio3 {
     public static void main(String[] args) throws Exception {
         Scanner scanner = new Scanner(System.in);
 
-        System.out.println("Ingrese la Base del triangulo: ");
+        System.out.println("Ingrese el radio del circulo: ");
         System.out.print("||");
-        float baseT = scanner.nextFloat();
 
-        System.out.println("Ingrese la Altura del triangulo: ");
-        System.out.print("||");
-        float alturaT = scanner.nextFloat();
+        double radio = scanner.nextDouble();
+        double area = Math.PI * Math.pow(radio, 2);
+        System.out.println("El área del círculo es: " + area);
 
-        float area = (baseT * alturaT) / 2;
-
-        System.out.println("El area del triangulo es: " + area);
+        scanner.close();
     }
 }

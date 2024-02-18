1f4c15e1fe165d4baa8d6574a4bb6a502d64a436
Daniel Valle <danielvalleortiz@gmail.com>
2024-02-18 16:51:23 -0500

    Ejercicio6

---
+++ Ejercicio6.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+public class Ejercicio6 {
+    public static void main(String[] args) throws Exception {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Ingrese 3 numeros para calcular su media aritmetica");
+        System.out.println("");
+
+        System.out.println("numero 1: ");
+        System.out.print("||");
+        float num1 = scanner.nextFloat();
+
+        System.out.println("numero 2: ");
+        System.out.print("||");
+        float num2 = scanner.nextFloat();
+
+        System.out.println("numero 3: ");
+        System.out.print("||");
+        float num3 = scanner.nextFloat();
+
+        float media = (num1 + num2 + num3) / 3;
+
+        System.out.println("La media aritmetica de los 3 numeros es: "+ media);
+    }
+}

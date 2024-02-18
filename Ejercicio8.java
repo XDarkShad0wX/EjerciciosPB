e0edb7d80e7baf2105e6c1d59179f978e7f6e7d0
Daniel Valle <danielvalleortiz@gmail.com>
2024-02-18 17:11:29 -0500

    Ejercicio8

---
+++ Ejercicio8.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+public class Ejercicio8 {
+    public static void main(String[] args) throws Exception {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("/// Ingrese dos numero para dividirlos y sacar el residuo ///");
+        System.out.println("");
+
+        System.out.println("primer numero: ");
+        System.out.print("||");
+        int num1 = scanner.nextInt();
+
+        System.out.println("Entre Segundo numero: ");
+        System.out.print("||");
+        int num2 = scanner.nextInt();
+
+        float residuo = num1 % num2;
+
+        System.out.println("El residuo de la division de " + num1 + " entre " + num2 + " es: " + residuo);
+
+        scanner.close();
+    }
+}

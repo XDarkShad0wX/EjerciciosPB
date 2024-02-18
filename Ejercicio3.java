0a808ef161ad70d816ceb1aae08c97684846a315
Daniel Valle <danielvalleortiz@gmail.com>
2024-02-18 16:36:32 -0500

    Ejercicio3

--- Ejercicio3.java
+++ Ejercicio3.java
@@ -5,9 +5,10 @@ public class Ejercicio3 {
         Scanner scanner = new Scanner(System.in);
 
         System.out.println("Ingrese la base del Triangulo: ");
-        System.out.println("||");
+        System.out.print("||");
         float baseT = scanner.nextFloat();
         System.out.println("Ingrede la altura del Triangulo: ");
+        System.out.print("||");
         float alturaT = scanner.nextFloat();
 
         float area = (baseT * alturaT) / 2;

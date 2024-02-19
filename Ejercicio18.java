c01317abd4905f5214038cf4e400dff4d52782db
Daniel Valle <danielvalleortiz@gmail.com>
2024-02-18 21:24:56 -0500

    Ejercicio18

---
+++ Ejercicio18.java
@@ -0,0 +1,8 @@
+public class Ejercicio18 {
+    public static void main(String[] args) throws Exception {
+
+        int num = (int) Math.pow(125, 2);
+
+        System.out.println("El cuadrado de 125 es: " + num);
+    }
+}

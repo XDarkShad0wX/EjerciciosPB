5132be694ed2d914dae55b9bd1b8e483e7c405cc
Daniel Valle <danielvalleortiz@gmail.com>
2024-02-28 00:17:19 -0500

    Ejercicio30

--- Ejercicio30.java
+++ Ejercicio30.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 public class Ejercicio30 {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception{
         Scanner scanner = new Scanner(System.in);
 
         System.out.print("Ingrese la distancia en kilometros: ");

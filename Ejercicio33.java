d95e2dbac0599c69b06f166073246c73090a4731
Daniel Valle <danielvalleortiz@gmail.com>
2024-03-03 01:08:54 -0500

    Ejercicio33

---
+++ Ejercicio33.java
@@ -0,0 +1,77 @@
+import java.util.Scanner;
+
+public class Ejercicio33 {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int contadorHom = 0;
+        int contadorMuj = 0;
+        int sumaSalarios = 0;
+        int salarioM700 = 0;
+        int contadorEmpl = 1;
+
+        System.out.println("");
+        System.out.println("Ingrese el número de empleados: ");
+        System.out.print("||");
+
+        while (!scanner.hasNextInt()) {
+            System.out.println("");
+            System.out.println("- Ingrese un número entero -");
+            System.out.print("||");
+
+            scanner.next();
+        }
+
+        int numeroE = scanner.nextInt();
+
+        while (contadorEmpl <= numeroE) {
+
+            System.out.println("");
+            System.out.println("Ingrese el genero del empleado #" + contadorEmpl + " (Hombre - Mujer)");
+            System.out.print("||");
+
+            String genero = scanner.next();
+
+            if (genero.equalsIgnoreCase("Hombre")) {
+                contadorHom++;
+            } else if (genero.equalsIgnoreCase("Mujer")) {
+                contadorMuj++;
+            }
+
+            System.out.println("");
+            System.out.println("Ingrese el Salario del empleado");
+            System.out.print("||");
+
+            while (!scanner.hasNextInt()) {
+                System.out.println("");
+                System.out.println("- Ingrese un numero Entero -");
+                System.out.print("||");
+
+                scanner.next();
+            }
+
+            int salarioE = scanner.nextInt();
+
+            if (salarioE > 700) {
+                salarioM700++;
+            }
+
+            sumaSalarios += salarioE;
+            contadorEmpl++;
+        }
+
+        double promedioSalarios = (double) sumaSalarios / numeroE;
+        double porcentajeMujeres = ((double) contadorMuj / numeroE) * 100;
+        double porcentajeHombres = ((double) contadorHom / numeroE) * 100;
+
+        System.out.println("");
+        System.out.println(".............................................................");
+        System.out.println("- Cantidad de personas que ganan más de 700: " + salarioM700);
+        System.out.println("- Promedio de Salarios: " + promedioSalarios);
+        System.out.println("- Porcentaje de Mujeres: " + porcentajeMujeres);
+        System.out.println("- Porcentaje de Hombres: " + porcentajeHombres);
+
+        scanner.close();
+    }
+}

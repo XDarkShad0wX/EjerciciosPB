7e02b1782cfb4db09871c27997bc4f0f528787fe
Daniel Valle <danielvalleortiz@gmail.com>
2024-03-26 02:13:12 -0500

    Ejercicio54

---
+++ Ejercicio54.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class Ejercicio54 {
+    public static void main(String[] args) throws Exception {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Ingrese una longitud en centimetros para comvertir en pulgadas: ");
+        System.out.print("||");
+        float longitud = scanner.nextFloat();
+
+        float centimetros = 2.54f;
+
+        float pulgadas = longitud / centimetros;
+
+        System.out.println("- " + longitud + " Centimetros es: " + pulgadas + " pulgadas");
+
+        scanner.close();
+    }
+}

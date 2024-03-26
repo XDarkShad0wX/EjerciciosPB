f1044480c96148b375608ce49a70f72fff9f198b
Daniel Valle <danielvalleortiz@gmail.com>
2024-03-26 02:14:17 -0500

    Ejercicio54

--- Ejercicio54.java
+++ Ejercicio54.java
@@ -11,6 +11,7 @@ public class Ejercicio54 {
 
         float pulgadas = longitud / centimetros;
 
+        System.out.println("");
         System.out.println("- " + longitud + " Centimetros es: " + pulgadas + " pulgadas");
 
         scanner.close();

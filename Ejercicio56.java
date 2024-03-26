64f4bbe1f6870bdc7d1d940934d8b039badb14fd
Daniel Valle <danielvalleortiz@gmail.com>
2024-03-26 13:55:29 -0500

    Ejercicio56

--- Ejercicio56.java
+++ Ejercicio56.java
@@ -3,6 +3,7 @@ public class Ejercicio56 {
     public static void main(String[] args) throws Exception {
         Scanner scanner = new Scanner(System.in);
 
+        System.out.println("");
         System.out.println("Ingrese un tiempo en segundo");
         System.out.print("||");
         float seg = scanner.nextFloat();
@@ -10,6 +11,7 @@ public class Ejercicio56 {
         float minutos = seg / 60;
         float horas = seg / 3600;
 
+        System.out.println("");
         System.out.println("El tiempo " + seg + " Segundos equivale a: ");
         System.out.println("- Minutos: " + minutos);
         System.out.println("- Horas: " + horas);

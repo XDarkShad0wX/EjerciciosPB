728edbe6d405ca7cc1bd88bcd48a1f6f72bc8f4b
Daniel Valle <danielvalleortiz@gmail.com>
2024-03-26 15:36:54 -0500

    Ejercicio60

--- Ejercicio60.java
+++ Ejercicio60.java
@@ -3,25 +3,24 @@ import java.util.Scanner;
 public class Ejercicio60 {
     public static void main(String[] args) throws Exception {
         Scanner scanner = new Scanner(System.in);
-        
+
         int i = 0;
-        
         int num;
-        
+
         do {
             System.out.println("");
-            System.out.println("// Ingrese un numero (0 para finalizar el programa) //");
+            System.out.println("// Ingrese cualquier numero (0 para finalizar el programa) //");
             System.out.print("|| ");
             num = scanner.nextInt();
-            
+
             if (num > 0) {
                 i++;
             }
-            
+
         } while(num != 0);
-        
+
         System.out.println("La cantidad de numeros ingesados mayores a 0 fueron: " + i);
-        
+
         scanner.close();
     }
 }

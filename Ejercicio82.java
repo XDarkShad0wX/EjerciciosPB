5f5db0f2098aa8123964696b4c086fe91f8fa9f8
Daniel Valle <danielvalleortiz@gmail.com>
2024-04-16 16:34:35 -0500

    Ejercicio82

--- Ejercicio82.java
+++ Ejercicio82.java
@@ -4,20 +4,21 @@ public class Ejercicio82 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        // Solicitar al usuario que ingrese un número
-        System.out.print("Ingrese un número: ");
+        System.out.println("Ingrese un número: ");
+        System.out.print("||");
         double numero = scanner.nextDouble();
 
-        // Cerrar el scanner
-        scanner.close();
-
-        // Verificar si el número es positivo, negativo o cero
         if (numero > 0) {
-            System.out.println("El número ingresado es positivo.");
+            System.out.println("");
+            System.out.println("El numero " + numero + " es positivo");
         } else if (numero < 0) {
-            System.out.println("El número ingresado es negativo.");
+            System.out.println("");
+            System.out.println("El numero " + numero + " es negativo.");
         } else {
-            System.out.println("El número ingresado es cero.");
+            System.out.println("");
+            System.out.println("El numero " + numero + " es cero.");
         }
+
+        scanner.close();
     }
 }

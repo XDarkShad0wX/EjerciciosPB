644046a73ef849d13b439df5d10b74befaed2684
Daniel Valle <danielvalleortiz@gmail.com>
2024-04-15 22:49:54 -0500

    Ejercicio77

--- Ejercicio77.java
+++ Ejercicio77.java
@@ -4,15 +4,19 @@ public class Ejercicio77 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        System.out.println("");
         System.out.println("Ingrese el capital del préstamo: ");
         System.out.print("||");
         double capital = scanner.nextDouble();
 
+        System.out.println("");
         System.out.println("Ingrese el interés anual (en porcentaje): ");
         System.out.print("||");
         double interesAnual = scanner.nextDouble();
 
+        System.out.println("");
         System.out.print("Ingrese el número de años: ");
+        System.out.print("||");
         int numAnios = scanner.nextInt();
 
         double interesMensual = interesAnual / (12 * 100);
@@ -20,6 +24,7 @@ public class Ejercicio77 {
         double cuotaMensual = (interesMensual * capital) / (1 - Math.pow(1 + interesMensual, -numPagos));
         double totalPagar = cuotaMensual * numPagos;
 
+        System.out.println("");
         System.out.println("El pago mensual de la hipoteca es: $" + cuotaMensual);
         System.out.println("El total a pagar es: $" + totalPagar);
 

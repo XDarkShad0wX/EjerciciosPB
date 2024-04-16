c47dd13d20ce0bdfb1eb852e205f495f8a16760a
Daniel Valle <danielvalleortiz@gmail.com>
2024-04-15 23:24:24 -0500

    Ejercicio79

--- Ejercicio79.java
+++ Ejercicio79.java
@@ -4,16 +4,16 @@ public class Ejercicio79 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        System.out.println("Ingrese el voltaje en voltios para el bombillo:");
-        double voltajeBombillo = scanner.nextDouble();
+        System.out.print("Ingrese el voltaje en voltios para el bombillo");
+        System.out.println("||");
+        double voltaje = scanner.nextDouble();
+
+        double voltajeK = voltaje / 1000;
+        double voltajeTotal = 3 * voltajeK;
+
+        System.out.println("");
+        System.out.println("El voltaje total de los tres bombillos es: " + voltajeTotal + " kilovoltios");
 
         scanner.close();
-
-        // Convertir voltaje de bombillo a kilovoltios
-        double voltajeKilovoltios = voltajeBombillo / 1000;
-
-        // Calcular y visualizar el voltaje total de los tres bombillos en kilovoltios
-        double voltajeTotal = 3 * voltajeKilovoltios;
-        System.out.println("El voltaje total de los tres bombillos es: " + voltajeTotal + " kilovoltios.");
     }
 }

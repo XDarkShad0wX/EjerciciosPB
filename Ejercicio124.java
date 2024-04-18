d0ec4aeb6b65f3b68ac341417d09ce467faceb17
Daniel Valle <danielvalleortiz@gmail.com>
2024-04-17 19:20:45 -0500

    Ejercicio124

---
+++ Ejercicio124.java
@@ -0,0 +1,12 @@
+public class Ejercicio124 {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 3000; i++) {
+
+            if (i % 3 == 0) {
+                System.out.println("");
+                System.out.println(i);
+            }
+
+        }
+    }
+}

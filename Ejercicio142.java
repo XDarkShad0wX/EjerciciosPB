9b55b043d684f0ab6ca51b371845f40c2cbd3ccd
Daniel Valle <danielvalleortiz@gmail.com>
2024-04-18 22:18:41 -0500

    Ejercicio142

--- Ejercicio142.java
+++ Ejercicio142.java
@@ -4,31 +4,30 @@ public class Ejercicio142 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        // Solicitar al usuario que ingrese el número base y el exponente
-        System.out.print("Ingrese el número base: ");
+        System.out.println("Ingrese el numero de la base: ");
+        System.out.print("||");
         double base = scanner.nextDouble();
-        System.out.print("Ingrese el exponente: ");
+
+        System.out.println("");
+        System.out.println("Ingrese el exponente: ");
+        System.out.println("||");
         int exponente = scanner.nextInt();
 
-        // Verificar si el exponente es negativo
         boolean esNegativo = exponente < 0;
         exponente = Math.abs(exponente);
 
-        // Calcular la potencia utilizando un bucle
         double resultado = 1;
         for (int i = 0; i < exponente; i++) {
             resultado *= base;
         }
 
-        // Si el exponente es negativo, calcular la inversa de la potencia
         if (esNegativo) {
             resultado = 1 / resultado;
         }
 
-        // Mostrar el resultado
-        System.out.println(base + " elevado a la potencia " + exponente + " es igual a: " + resultado);
+        System.out.println("");
+        System.out.println("El numero " + base + " elevado a la potencia " + exponente + " es igual a: " + resultado);
 
-        // Cerrar el scanner
         scanner.close();
     }
 }
